c2a1ace0d65d2f642b55c51a01682820bca27c17
numoonchld <raghavendra.saralaya@gmail.com>
2021-12-16 17:08:41 +0530

    be and all matchers

--- spec/be_matcher_spec.rb
+++ spec/be_matcher_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe 'be matchers' do
     expect({}).to be_truthy
     expect(:symbol).to be_truthy
   end
+
+  it 'can test for falsy' do
+    expect(false).to be_falsy
+    expect(nil).to be_falsy
+  end
+
+  it 'can test for nil' do
+    expect(nil).to be_nil
+
+    my_hash = { a: 5 }
+    expect(my_hash[:b]).to be_nil
+
+  end
 end
